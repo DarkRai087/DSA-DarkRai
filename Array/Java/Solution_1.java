c481360f2b2b611503ca793f17b7b9332fd9576b
Parth Pandey <parthp90111@gmail.com>
2024-11-10 18:04:18 +0530

    java q1

---
+++ Array/Java/Solution_1.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Solution_1 {
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 9, 1, 7 };
+
+        int max = Arrays.stream(arr).max().orElse(Integer.MIN_VALUE);
+        int min = Arrays.stream(arr).min().orElse(Integer.MAX_VALUE);
+
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    }
+}
