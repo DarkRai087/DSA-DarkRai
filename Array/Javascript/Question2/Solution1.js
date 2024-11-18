3ebc00473859eae8dc9ff167756a2270438361af
Parth Pandey <parthp90111@gmail.com>
2024-11-18 10:11:38 +0530

    cr

--- Array/Javascript/Question2/Solution1.js
+++ Array/Javascript/Question2/Solution1.js
@@ -1,5 +1,5 @@
 let arr2 = [1, 2, 3, 4, 5];
-// Swap elements in place using two pointers
+
 let left = 0, right = arr2.length - 1;
 while (left < right) {
   [arr2[left], arr2[right]] = [arr2[right], arr2[left]];
