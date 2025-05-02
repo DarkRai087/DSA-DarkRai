4ecb23c2055dcaa1ef6d287e3c430624182cc7b0
Parth Pandey <parthp90111@gmail.com>
2025-05-02 09:10:07 +0530

    3 Q

---
+++ Straight-100 js/maximum_e.js
@@ -0,0 +1,13 @@
+// Find the maximum element in an array.
+
+let arr = [3, 1, 4, 1, 5, 9, 2, 6, 5]; // Maximum element is 9
+let arr2 = [-10, -20, -30, -5, -1]; // Maximum element is -1
+
+
+function maximum(arr){
+   return Math.max(...arr)
+}
+console.log(maximum(arr2));
+
+
+
